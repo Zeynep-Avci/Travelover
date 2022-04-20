2433039f2b078a850801361eec84495c84df69be
Zeynep-Avci <zeynep.avci.66@hotmail.com>
2022-04-20 11:08:54 +0200

    Blank Screen

--- app/src/main/java/com/example/travelover/MainActivity.kt
+++ app/src/main/java/com/example/travelover/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.travelover
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.travelover.ui.theme.TraveloverTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +22,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             TraveloverTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colors.background) {
+                    HomeScreen()
                 }
             }
         }
@@ -30,14 +31,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HomeScreen() {
+
+    Surface( modifier = Modifier
+        // Modifier Attribute .fillMaxWidth() und .fillMaxHeight() füllen den gesamten Surface
+        .fillMaxWidth()
+        .fillMaxHeight()) {
+        Card(modifier = Modifier
+            .width(200.dp)
+            .height(390.dp)
+            .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            elevation = 4.dp) {
+            // was von oben nach unten kommt
+            Column(modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TraveloverTheme {
-        Greeting("Android")
+        HomeScreen()
     }
 }
