3d838cd5747840470b5259bb68a2ddb9d5a51f1c
Zeynep-Avci <zeynep.avci.66@hotmail.com>
2022-04-20 11:36:27 +0200

    Scaffold

--- app/src/main/java/com/example/travelover/MainActivity.kt
+++ app/src/main/java/com/example/travelover/MainActivity.kt
@@ -3,15 +3,19 @@ package com.example.travelover
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.travelover.ui.theme.TraveloverTheme
@@ -48,10 +52,34 @@ fun HomeScreen() {
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally) {
 
+                Scaffold(
+                    topBar = {
+                        TopAppBar(backgroundColor = Color.Magenta, elevation = 3.dp) {
+                            Row {
+                                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Arrow back",
+                                    modifier = Modifier.clickable {
+                                        //navController.popBackStack()  // go back to last screen
+                                    })
+
+                                Spacer(modifier = Modifier.width(20.dp))
+                                Text(text = "My Favorite Movies", style = MaterialTheme.typography.h6)
+                            }
+
+                        }
+
+                    }
+                ){
+                    MainContent()
+                }
+
             }
         }
     }
 }
+
+@Composable
+fun MainContent
+
 
 @Preview(showBackground = true)
 @Composable
