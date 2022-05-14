2480580361561c027fc546202cc625f88defc496
Zeynep-Avci <zeynep.avci.66@hotmail.com>
2022-05-14 20:49:54 +0200

    MainActivity fixed

--- app/src/main/java/com/example/travelover/MainActivity.kt
+++ app/src/main/java/com/example/travelover/MainActivity.kt
@@ -7,19 +7,21 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.travelover.Navigation.CityNavigation
+import com.example.travelover.ui.theme.TraveloverTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Travelover {
+            TraveloverTheme() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     //modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                   CityNavigation()
+                    CityNavigation()
                 }
             }
         }
@@ -30,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Travelover {
+    TraveloverTheme {
         CityNavigation()
     }
 }
