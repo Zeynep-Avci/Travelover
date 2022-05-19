8a667958959c02637b8eda2494f7c7a1db9734aa
lights9 <nursarhan@gmail.com>
2022-05-19 11:29:02 +0200

    Changed Homescreen View, Image Cards

--- app/src/main/java/com/example/travelover/Widgets/AppWidgets.kt
+++ app/src/main/java/com/example/travelover/Widgets/AppWidgets.kt
@@ -2,6 +2,7 @@ package com.example.travelover.Widgets
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
@@ -17,17 +18,22 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.travelover.Models.City
 import com.example.travelover.Models.getCities
 
 @Composable
-fun SimpleTopAppBar(arrowBackClicked: () -> Unit = {}, content: @Composable () -> Unit){
+fun SimpleTopAppBar(arrowBackClicked: () -> Unit = {}, content: @Composable () -> Unit) {
     TopAppBar(elevation = 3.dp) {
         Row {
             Icon(imageVector = Icons.Default.ArrowBack,
@@ -50,7 +56,7 @@ fun FavoriteIcon(
     city: City = getCities()[0],
     isFav: Boolean = false,
     onFavClicked: (City) -> Unit = {},
-){
+) {
     IconButton(
         modifier = Modifier.width(80.dp),
         onClick = { onFavClicked(city) }
@@ -60,7 +66,9 @@ fun FavoriteIcon(
             imageVector =
             if (isFav) Icons.Default.Favorite
             else Icons.Default.FavoriteBorder,
-            contentDescription = "add to favorites")
+            contentDescription = "add to favorites",
+
+            )
     }
 }
 
@@ -77,91 +85,65 @@ fun CityRow(
         mutableStateOf(false)
     }
 
-    Card(modifier = Modifier
-        .padding(4.dp)
-        .fillMaxWidth()
-        .clickable {
-            onItemClick(city.id)
-        },
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .clickable
+            {
+                onItemClick(city.id)
+            },
         shape = RoundedCornerShape(corner = CornerSize(16.dp)),
-        elevation = 6.dp) {
+        elevation = 6.dp
+    ) {
 
-        Column() {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ){
+        Box(modifier = Modifier.height(200.dp)) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(city.images[0])
+                    .crossfade(true)
+                    .build(),
+                contentDescription = "City image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.clip(RectangleShape)
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                androidx.compose.ui.graphics.Color.Transparent,
+                                androidx.compose.ui.graphics.Color.Black
+                            ),
+                            startY = 200f
 
-                Surface(modifier = Modifier
-                    .padding(12.dp)
-                    .size(100.dp)) {
-
-                    AsyncImage(
-                        model = ImageRequest.Builder(LocalContext.current)
-                            .data(city.images[0])
-                            .crossfade(true)
-                            .build(),
-                        contentDescription = "City poster",
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier.clip(CircleShape)
+                        )
                     )
-                }
-
-                Column(modifier = Modifier
-                    .padding(4.dp)
-                    .widthIn(200.dp, 200.dp)) {
-                    Text(text = city.name,
-                        style = MaterialTheme.typography.h6)
-
-                    Text(text = "Area ${city.area}",
-                        style = MaterialTheme.typography.caption)
-
-                    Text(text = "Lanuguage: ${city.language}",
-                        style = MaterialTheme.typography.caption)
-
-                    AnimatedVisibility(visible = expanded) {
-                        Column {
-                            Text(text = "Country: ${city.country}", style = MaterialTheme.typography.caption)
-
-                            Divider(modifier = Modifier.padding(3.dp))
-
-                            Text(text = "Population: ${city.population}", style = MaterialTheme.typography.caption)
-                            Text(text = "Language: ${city.language}", style = MaterialTheme.typography.caption)
-                            Text(text = "Area: ${city.area}", style = MaterialTheme.typography.caption)
-                        }
-                    }
-
-
-                    Icon(imageVector =
-                    if (expanded) Icons.Filled.KeyboardArrowDown
-                    else Icons.Filled.KeyboardArrowUp,
-                        contentDescription = "expand",
-                        modifier = Modifier
-                            .size(25.dp)
-                            .clickable {
-                                expanded = !expanded
-                            },
-                        tint = androidx.compose.ui.graphics.Color.DarkGray)
-                }
-
-                content()
-                /*
-                IconButton(
-                    modifier = Modifier.width(80.dp),
-                    onClick = { onFavClicked(city) }
-                ) {
-                    Icon(
-                        tint = MaterialTheme.colors.secondary,
-                        imageVector =
-                        if (isFav) Icons.Default.Favorite
-                        else Icons.Default.FavoriteBorder,
-                        contentDescription = "add to favorites")
-                }
-                 */
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(3.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = city.name,
+                    style = TextStyle(
+                        color = androidx.compose.ui.graphics.Color.White,
+                        fontSize = 55.sp
+                    )
+                )
             }
+
+
         }
+
+        content()
+
     }
 }
+
 
 @Composable
 fun HorizontalScrollableImageView(city: City) {
