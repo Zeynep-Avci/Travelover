d9bea03d0897475475ef447645e48245ff39cf64
lights9 <nursarhan@gmail.com>
2022-06-08 10:11:42 +0200

    Added three more cities

--- app/src/main/java/com/example/travelover/Models/City.kt
+++ app/src/main/java/com/example/travelover/Models/City.kt
@@ -4,9 +4,6 @@ data class City(
     val id: String,
     val name: String,
     val country: String,
-    val population: String,
-    val area: String,
-    val language: String,
     val images: List<String>,
     val sights: List<String>,
 )
@@ -15,30 +12,25 @@ fun getCities(): List<City> {
     return listOf(
         City(
             id = "1",
-            name = "Vienna",
-            country = "Austria",
-            population = "2.600.000",
-            area = "414.78 km2",
-            language = "German",
+            name = "Cairo",
+            country = "Egypt",
             images = listOf(
-                "https://www.goodviennatours.eu/wp-content/uploads/2022/02/AdobeStock_268148289-scaled.jpeg",
-                "https://theplanetd.com/images/Things-to-do-in-Vienna-Austria.jpg",
-                "https://theplanetd.com/images/Things-to-do-in-Vienna-Austria.jpg"
+                "https://idsb.tmgrup.com.tr/ly/uploads/images/2020/04/17/thumbs/800x531/31299.jpg",
+                "https://i.pinimg.com/originals/ea/b3/57/eab35737bc2986c200c576511324237d.jpg",
+                "https://istanbul-tourist-information.com/wp-content/uploads/2021/02/About-Istanbul.jpg"
             ),
             sights = listOf(
                 "sight1",
                 "sight2",
                 "sight3"
             ),
-        ),
+
+            ),
 
         City(
             id = "2",
             name = "Milan",
             country = "Italy",
-            population = "1.399.860",
-            area = "181.76 km2",
-            language = "Italian",
             images = listOf(
                 "https://fempop.ch/wp-content/uploads/2019/11/Milano-Dom.jpg",
                 "https://image-service.web.oebb.at/www.nightjet.com/.imaging/default/dam/nightjet/bildergalerie/laender-2560x1600/italien/mailand-galleria-vittorio2.jpg/jcr:content.jpg?t=1623680072873&scale=0.5",
@@ -54,19 +46,70 @@ fun getCities(): List<City> {
 
         City(
             id = "3",
-            name = "Istanbul",
-            country = "Turkey",
-            population = "15.840.900",
-            area = "5.343.22 km2",
-            language = "Turkish",
+            name = "Vienna",
+            country = "Austria",
             images = listOf(
-                "https://idsb.tmgrup.com.tr/ly/uploads/images/2020/04/17/thumbs/800x531/31299.jpg",
-                "https://i.pinimg.com/originals/ea/b3/57/eab35737bc2986c200c576511324237d.jpg",
-                "https://istanbul-tourist-information.com/wp-content/uploads/2021/02/About-Istanbul.jpg"),
+                "https://www.goodviennatours.eu/wp-content/uploads/2022/02/AdobeStock_268148289-scaled.jpeg",
+                "https://theplanetd.com/images/Things-to-do-in-Vienna-Austria.jpg",
+                "https://theplanetd.com/images/Things-to-do-in-Vienna-Austria.jpg"
+            ),
             sights = listOf(
                 "sight1",
                 "sight2",
-                "sight3"),),
+                "sight3"
+            ),
+
+            ),
+
+
+        City(
+            id = "4",
+            name = "Istanbul",
+            country = "Turkey",
+            images = listOf(
+                "https://idsb.tmgrup.com.tr/ly/uploads/images/2020/04/17/thumbs/800x531/31299.jpg",
+                "https://i.pinimg.com/originals/ea/b3/57/eab35737bc2986c200c576511324237d.jpg",
+                "https://istanbul-tourist-information.com/wp-content/uploads/2021/02/About-Istanbul.jpg"
+            ),
+            sights = listOf(
+                "sight1",
+                "sight2",
+                "sight3"
+            ),
+        ),
+
+
+            City(
+                id = "5",
+                name = "Venice",
+                country = "Italy",
+                images = listOf(
+                    "https://idsb.tmgrup.com.tr/ly/uploads/images/2020/04/17/thumbs/800x531/31299.jpg",
+                    "https://i.pinimg.com/originals/ea/b3/57/eab35737bc2986c200c576511324237d.jpg",
+                    "https://istanbul-tourist-information.com/wp-content/uploads/2021/02/About-Istanbul.jpg"
+                ),
+                sights = listOf(
+                    "sight1",
+                    "sight2",
+                    "sight3"
+                ),
+            ),
+
+            City(
+                id = "6",
+                name = "Kyoto",
+                country = "Japan",
+                images = listOf(
+                    "https://idsb.tmgrup.com.tr/ly/uploads/images/2020/04/17/thumbs/800x531/31299.jpg",
+                    "https://i.pinimg.com/originals/ea/b3/57/eab35737bc2986c200c576511324237d.jpg",
+                    "https://istanbul-tourist-information.com/wp-content/uploads/2021/02/About-Istanbul.jpg"
+                ),
+                sights = listOf(
+                    "sight1",
+                    "sight2",
+                    "sight3"
+                ),
+            ),
 
         )
 
