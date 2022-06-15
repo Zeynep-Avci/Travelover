b7f752515a27c00f16b3003d5a6fc407ecac83f8
Zeynep-Avci <zeynep.avci.66@hotmail.com>
2022-06-15 11:22:33 +0200

    - SplashScreen was implemented

--- app/src/main/java/com/example/travelover/MainActivity.kt
+++ app/src/main/java/com/example/travelover/MainActivity.kt
@@ -3,17 +3,32 @@ package com.example.travelover
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.travelover.Navigation.CityNavigation
+import com.example.travelover.ViewModels.SplashScreenViewModel
 import com.example.travelover.ui.theme.TraveloverTheme
 
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: SplashScreenViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Thread.sleep(50)
+        //val splashScreen = installSplashScreen()
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                viewModel.isLoading.value
+            }
+        }
+
         setContent {
             TraveloverTheme() {
                 // A surface container using the 'background' color from the theme
