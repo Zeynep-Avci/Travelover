8d7b4c5401ad4a393804ca0351321e7592de1f96
lights9 <nursarhan@gmail.com>
2022-06-15 11:59:26 +0200

    Implemented SightsScreen with pictures, still has to be fixed

--- app/src/main/java/com/example/travelover/MainActivity.kt
+++ app/src/main/java/com/example/travelover/MainActivity.kt
@@ -9,14 +9,18 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+//import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.travelover.Navigation.CityNavigation
 import com.example.travelover.ViewModels.SplashScreenViewModel
+import com.example.travelover.Widgets.SightsImageSlider
 import com.example.travelover.ui.theme.TraveloverTheme
+import com.google.accompanist.pager.ExperimentalPagerApi
 
 
 class MainActivity : ComponentActivity() {
 
     private val viewModel: SplashScreenViewModel by viewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +41,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     CityNavigation()
+                    //SightsImageSlider()
                 }
             }
         }
--- app/src/main/java/com/example/travelover/Widgets/AppWidgets.kt
+++ app/src/main/java/com/example/travelover/Widgets/AppWidgets.kt
@@ -1,17 +1,21 @@
 package com.example.travelover.Widgets
 
 
+import android.graphics.PorterDuff
+import android.widget.RatingBar
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
@@ -22,22 +26,26 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.lerp
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.util.lerp
+import androidx.compose.ui.viewinterop.AndroidView
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
-import com.example.travelover.Models.City
-import com.example.travelover.Models.Sight
-import com.example.travelover.Models.getCities
-import com.example.travelover.Models.getSights
+import com.example.travelover.Models.*
 import com.example.travelover.R
+import com.example.travelover.ui.theme.LogoPink
+import com.google.accompanist.pager.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.yield
 import kotlin.math.absoluteValue
+
 
 @Composable
 fun SimpleTopAppBar(arrowBackClicked: () -> Unit = {}, content: @Composable () -> Unit) {
@@ -184,84 +192,170 @@ fun HorizontalScrollableImageView(city: City) {
     }
 }
 
+
+//@ExperimentalPagerApi
 @Composable
-fun HorizontalScrollableSightsView(
-    sight: Sight
-) {
+fun SightsImageSlider(sight: Sight) {
 
-    LazyRow {
-        items(sight.sight_images) { image ->
-            Card(
-                //border = BorderStroke(1.dp, androidx.compose.ui.graphics.Color.Black) ,
-                modifier = Modifier
-                    .padding(4.dp)
-                    .fillMaxHeight()
-                    .fillMaxWidth(),
+    /* val pagerState = rememberPagerState(
+         pageCount = natural.size,
+         initialPage = 2
+     )
 
+     LaunchedEffect(Unit) {
+         while (true) {
+             yield()
+             delay(2000)
+             pagerState.animateScrollToPage(
+                 page = (pagerState.currentPage + 1) % (pagerState.pageCount),
+                 animationSpec = tween(600)
+             )
+         }
+     }*/
 
-                shape = RoundedCornerShape(corner = CornerSize(16.dp)),
-                elevation = 6.dp
-            ) {
-
-                Box(modifier = Modifier.height(200.dp)) {
-                    AsyncImage(
-                        model = ImageRequest.Builder(LocalContext.current)
-                            .data(image)
-                            .crossfade(true)
-                            .build(),
-                        contentDescription = "City famous sight image",
-                        //contentScale = ContentScale.FillBounds,
-                        //modifier = Modifier.clip(RectangleShape)
-
-
-                    )
-                }
-                //content()
-            }
-        }
-    }
-}
-
-/*
-
-    Box(
+    Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        LazyRow(
-            state = listState, modifier = Modifier
-                .background(androidx.compose.ui.graphics.Color.Yellow),
-               // .wrapContentHeight()
-                //.fillMaxWidth(),
-            contentPadding = PaddingValues(12.dp),
-            //verticalArrangement = Arrangement.spacedBy(5.dp)
+        //column for title Box at the top
+        Column(
+            modifier = Modifier
+                .height(50.dp)
+                .fillMaxWidth()
+                .background(LogoPink),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            items(sight.sight_images) { image ->
-                Card(
-                    modifier = Modifier
-                        .padding(12.dp)
-                        .fillMaxHeight()
-                        .fillMaxWidth(),
-                    elevation = 4.dp
-                ) {
-                    Column(
-                        modifier = Modifier.padding(3.dp)
-                    ) {
-                        Row(verticalAlignment = Alignment.CenterVertically) {
-                            AsyncImage(
-                                model = ImageRequest.Builder(LocalContext.current)
-                                    .data(image)
-                                    .crossfade(true)
-                                    .build(),
-                                contentDescription = "City famous sight image",
-                                //contentScale = ContentScale.Crop
-                            )
-                        }
+            Text(
+                text = "Famous Sights",
+                color = Color.White,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+
+
+        /*HorizontalPager(
+            state = pagerState,
+            modifier = Modifier
+                .weight(1f)
+                .padding(0.dp, 40.dp, 0.dp, 40.dp)
+        ) { //page ->
+        Card(
+            modifier = Modifier
+                /*.graphicsLayer {
+                    val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
+                    lerp(
+                        start = 0.85f,
+                        stop = 1f,
+                        fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                    ).also { scale ->
+                        scaleX = scale
+                        scaleY = scale
                     }
 
+                }*/
+                .fillMaxWidth()
+                .padding(15.dp, 0.dp, 15.dp, 0.dp),
+            shape = RoundedCornerShape(20.dp)
+        ) {
+            //val natural = sight[page]
+            val displaySight = getSights()*/
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(LogoPink)
+                    .align(Alignment.CenterHorizontally)
+            ) {
 
+/*
+                    Image(
+                        painter = painterResource(
+                            id = when (page) {
+                                1 -> R.drawable.image_1
+                                2 -> R.drawable.image_2
+                                3 -> R.drawable.image_3
+                                4 -> R.drawable.image_4
+                                5 -> R.drawable.image_5
+                                else -> R.drawable.image_1
+                            }
+                        ),
+                        contentDescription = "Image",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )*/
+                LazyRow {
+                    items(sight.sight_images) { image ->
+                        Card(
+                            //border = BorderStroke(1.dp, androidx.compose.ui.graphics.Color.Black) ,
+                            modifier = Modifier
+                                .padding(6.dp)
+                                .fillMaxSize()
+                                .padding(15.dp, 0.dp, 15.dp, 0.dp),
+
+
+                            shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+                            elevation = 6.dp
+                        ) {
+
+                            Box(modifier = Modifier.height(450.dp).width(350.dp)) {
+                                AsyncImage(
+                                    model = ImageRequest.Builder(LocalContext.current)
+                                        .data(image)
+                                        .crossfade(true)
+                                        .build(),
+                                    contentDescription = "City famous sight image",
+                                    contentScale = ContentScale.Crop,
+                                    modifier = Modifier.fillMaxSize()
+
+
+
+                                )
+                            }
+                            //content()
+                        }
+                    }
+                }
+                Column(
+                    modifier = Modifier
+                        .align(Alignment.BottomStart)
+                        .padding(15.dp)
+                ) {
+                    Text(
+                        text = sight.sight_name,
+                        style = MaterialTheme.typography.h5,
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
+/*
+                    val ratingBar = RatingBar(
+                        LocalContext.current, null,// R.attr.ratingBarStyleSmall
+                    ).apply {
+                        rating = sight.rating
+                        progressDrawable.setColorFilter(
+                            android.graphics.Color.parseColor("#FF0000"),
+                            PorterDuff.Mode.SRC_ATOP
+                        )
+                    }
+                    AndroidView(
+                        factory = { ratingBar },
+                        modifier = Modifier.padding(0.dp, 8.dp, 0.dp, 0.dp)
+                    )*/
+
+                    Text(
+                        text = sight.desc,
+                        style = MaterialTheme.typography.body1,
+                        color = Color.White,
+                        fontWeight = FontWeight.Normal,
+                        modifier = Modifier.padding(0.dp, 8.dp, 0.dp, 0.dp)
+                    )
                 }
             }
         }
     }
-}*/
+
+
+
+
+
 
